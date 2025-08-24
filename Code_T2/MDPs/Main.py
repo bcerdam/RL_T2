2b3e72f58120bbc0ba0edaf9bb32f4449fbc1be2
bcerdam <bcmardini@gmail.com>
2025-08-24 17:42:01 -0400

    d) lista, iterative policy evaluation

--- Code_T2/MDPs/Main.py
+++ Code_T2/MDPs/Main.py
@@ -1,10 +1,27 @@
 import random
+import time
 import numpy as np
 import matplotlib.pyplot as plt
 
 from Problems.CookieProblem import CookieProblem
 from Problems.GridProblem import GridProblem
 from Problems.GamblerProblem import GamblerProblem
+
+
+def plot_convergence(plotting_data, labels, title):
+    plt.figure(figsize=(12, 8))
+
+    for i, deltas in enumerate(plotting_data):
+        plt.plot(deltas, label=labels[i])
+
+    plt.title(title, fontsize=16)
+    plt.xlabel("Iteration until convergence (While loop, delta < theta)", fontsize=12)
+    plt.ylabel("Delta", fontsize=12)
+    plt.grid(True, which="both", ls="--")
+    plt.legend()
+    plt.tight_layout()
+    # plt.savefig('pregunta_e_cookie.jpeg', dpi=500)
+    plt.show()
 
 
 def get_action_from_user(actions):
@@ -71,40 +88,101 @@ def play(problem, theta, gamma):
                 v_value = V_s[state]
                 V_s[state] = bellman(actions, problem, state, V_s, gamma)
                 delta = max(delta, np.abs(v_value - V_s[state]))
-                deltas.append(delta)
 
+        deltas.append(delta)
         if delta < theta:
             done = True
 
-    return deltas
+    return V_s, deltas
 
 
-def play_gambler_problem(theta, gamma):
-    p = 0.4
+def play_gambler_problem(p, theta, gamma):
     problem = GamblerProblem(p)
-    return play(problem, theta, gamma)
+    V_s, deltas = play(problem, theta, gamma)
+    return V_s, deltas, problem
 
 
-def play_grid_problem(theta, gamma):
-    size = 4
+def play_grid_problem(size, theta, gamma):
     problem = GridProblem(size)
-    return play(problem, theta, gamma)
+    V_s, deltas = play(problem, theta, gamma)
+    return V_s, deltas, problem
 
 
-def play_cookie_problem(theta, gamma):
-    size = 3
+def play_cookie_problem(size, theta, gamma):
     problem = CookieProblem(size)
-    return play(problem, theta, gamma)
+    V_s, deltas = play(problem, theta, gamma)
+    return V_s, deltas, problem
 
 
 if __name__ == '__main__':
 
+    '''
+    Pregunta d)
+    '''
     THETA = 0.0000000001
-    GAMMA = 0.99
 
-    # deltas = play_cookie_problem(THETA, GAMMA)
-    # deltas = play_grid_problem(THETA, GAMMA)
-    deltas = play_gambler_problem(THETA, GAMMA)
+    # print("--- GridProblem START ---")
+    #
+    # GAMMA = 1
+    # plotting_info = []
+    # for size in range(3, 11):
+    #     start_time = time.time()
+    #     V_s, deltas, problem = play_grid_problem(size, THETA, GAMMA)
+    #     duration = time.time() - start_time
+    #
+    #     plotting_info.append(deltas)
+    #     initial_state = problem.get_initial_state()
+    #     initial_state_value = V_s[initial_state]
+    #     print(f"Tamaño grilla: {size}; Valor estado inicial: {initial_state_value:.3f}; Tiempo Ejecucion: {duration:.3f}s")
+    #
+    # plot_labels = [f"Tamaño grilla {size}x{size}" for size in range(3, 11)]
+    # plot_convergence(plotting_info, plot_labels, "GridProblem: Iterative Policy Evaluation Convergence")
+    #
+    # print("--- GridProblem END ---")
 
-    plt.plot(deltas)
-    plt.show()
+    # print("--- CookieProblem START ---")
+    #
+    # GAMMA = 0.99
+    # plotting_info = []
+    # for size in range(3, 11):
+    #     start_time = time.time()
+    #     V_s, deltas, problem = play_cookie_problem(size, THETA, GAMMA)
+    #     duration = time.time() - start_time
+    #
+    #     plotting_info.append(deltas)
+    #     initial_state = problem.get_initial_state()
+    #     initial_state_value = V_s[initial_state]
+    #     print(
+    #         f"Tamaño grilla: {size}; Valor estado inicial: {initial_state_value:.3f}; Tiempo Ejecucion: {duration:.3f}s")
+    #
+    # plot_labels = [f"Tamaño grilla {size}x{size}" for size in range(3, 11)]
+    # plot_convergence(plotting_info, plot_labels, "CookieProblem: Iterative Policy Evaluation Convergence")
+    #
+    # print("--- CookieProblem END ---")
+
+    # print("--- GamblerProblem START ---")
+    #
+    # GAMMA = 1
+    # p_head = [0.25, 0.4, 0.55]
+    # plotting_info = []
+    # for p in p_head:
+    #
+    #
+    #     start_time = time.time()
+    #     V_s, deltas, problem = play_gambler_problem(p, THETA, GAMMA)
+    #     duration = time.time() - start_time
+    #
+    #     plotting_info.append(deltas)
+    #     initial_state = problem.get_initial_state()
+    #     initial_value = V_s[initial_state]
+    #
+    #     print(f"Probabilidad cara = {p}; Valor estado inicial: {initial_value:.3f}; Tiempo Ejecucion: {duration:.3f}s")
+    #
+    # plot_labels = [f"p={p}" for p in p_head]
+    # plot_convergence(plotting_info, plot_labels, "GamblerProblem: Iterative Policy Evaluation Convergence")
+    #
+    # print("--- GamblerProblem END ---")
+
+    '''
+    Pregunta d)
+    '''
